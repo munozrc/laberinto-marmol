5c63082a362ffcc8f58e418d1aa1132100f3a0f1
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-05-13 16:10:16 -0500

    Update code

--- src/main.ino
+++ src/main.ino
@@ -2,36 +2,67 @@
 
 #define NUM_ROWS 10
 #define NUM_COLUMNS 10
-#define DATA_PIN 8
+#define DATA_PIN 6
+#define JOYSTICK_X_PIN A0
+#define JOYSTICK_Y_PIN A1
 
 CRGB LEDs[NUM_ROWS * DATA_PIN];
+
+int playerPositionX = 0;
+int playerPositionY = 0;
 
 void setup()
 {
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(LEDs, NUM_ROWS * NUM_COLUMNS);
-  FastLED.setBrightness(30);
+  FastLED.setBrightness(10);
   FastLED.clear();
 }
+
+unsigned int speed = 100;
+unsigned long timestamp = 0;
 
 void loop()
 {
 
-  for (int i = 0; i < NUM_ROWS; i++)
+  int directionX = getAxisValue(JOYSTICK_X_PIN, 500, 160);
+  int directionY = getAxisValue(JOYSTICK_Y_PIN, 500, 160);
+
+  if (millis() > timestamp + speed)
   {
-    for (int j = 0; j < NUM_COLUMNS; j++)
+    timestamp = millis();
+
+    int posX = playerPositionX + directionX;
+    int posY = playerPositionY + directionY;
+
+    if (posX != playerPositionX || posY != playerPositionY)
     {
-      int ledPosition = getLedPosition(i, j);
+      LEDs[getLedPosition(playerPositionX, playerPositionY)] = CRGB::Black;
+    }
 
-      LEDs[ledPosition] = CRGB::Red;
-      FastLED.show();
-      delay(10);
+    if (posX <= 9 && posX >= 0)
+    {
+      playerPositionX += directionX;
+    }
 
-      LEDs[ledPosition] = CRGB::Black;
-      delay(10);
+    if (posY <= 9 && posY >= 0)
+    {
+      playerPositionY += directionY;
     }
   }
 
-  delay(500);
+  LEDs[getLedPosition(playerPositionX, playerPositionY)] = CRGB::Blue;
+  FastLED.show();
+}
+
+int getAxisValue(uint8_t pin, int originAxis, int axisThreshold)
+{
+  int value = 0;
+  int raw = analogRead(pin);
+
+  raw < originAxis - axisThreshold ? value = 1 : 0;
+  raw > originAxis + axisThreshold ? value = -1 : 0;
+
+  return value;
 }
 
 int getLedPosition(int x, int y)
