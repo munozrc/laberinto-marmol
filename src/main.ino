e311e87865bcb19b42e268c46b3ebc367f83b670
Carlos Muñoz <munozrengifocarlos@gmail.com>
2022-05-17 21:32:52 -0500

    feat: Add sensor MPU6050

--- src/main.ino
+++ src/main.ino
@@ -1,8 +1,21 @@
 #include <FastLED.h>
+#include <Wire.h>
+
+// Direccion I2C de la IMU
+#define MPU 0x68
+
+// Ratios de conversion
+#define A_R 16384.0 // 32768/2
+#define G_R 131.0   // 32768/250
+
+// Conversion de radianes a grados 180/PI
+#define RAD_A_DEG = 57.295779
 
 #define NUM_ROWS 10
 #define NUM_COLUMNS 10
+
 #define DATA_PIN 6
+
 #define JOYSTICK_X_PIN A0
 #define JOYSTICK_Y_PIN A1
 
@@ -12,6 +25,17 @@ CRGB playerColor = CRGB::Red;
 
 int playerPositionX = 2;
 int playerPositionY = 2;
+
+// MPU-6050 da los valores en enteros de 16 bits
+// Valores RAW
+int16_t AcX, AcY, AcZ, GyX, GyY, GyZ;
+
+// Angulos
+float Acc[2];
+float Gy[3];
+float Angle[3];
+
+float dt;
 
 int board[NUM_ROWS][NUM_COLUMNS] = {
     {5, 5, 5, 5, 5, 5, 5, 5, 5, 5},
@@ -30,16 +54,67 @@ void setup()
   FastLED.addLeds<WS2812B, DATA_PIN, GRB>(LEDs, NUM_ROWS * NUM_COLUMNS);
   FastLED.setBrightness(2);
   FastLED.clear();
+
+  Wire.begin(); // A4=SDA / A5=SCL
+  Wire.beginTransmission(MPU);
+  Wire.write(0x6B);
+  Wire.write(0);
+  Wire.endTransmission(true);
+
+  Serial.begin(9600);
 }
 
 unsigned int speed = 100;
 unsigned long timestamp = 0;
+unsigned long timestampWire = 0;
 
 void loop()
 {
 
-  int directionX = getAxisValue(JOYSTICK_X_PIN, 500, 160);
-  int directionY = getAxisValue(JOYSTICK_Y_PIN, 500, 160);
+  // Leer los valores del Acelerometro de la IMU
+  Wire.beginTransmission(MPU);
+  Wire.write(0x3B); // Pedir el registro 0x3B - corresponde al AcX
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 6, true);       // A partir del 0x3B, se piden 6 registros
+  AcX = Wire.read() << 8 | Wire.read(); // Cada valor ocupa 2 registros
+  AcY = Wire.read() << 8 | Wire.read();
+  AcZ = Wire.read() << 8 | Wire.read();
+
+  // A partir de los valores del acelerometro, se calculan los angulos Y, X
+  // respectivamente, con la formula de la tangente.
+  Acc[1] = atan(-1 * (AcX / A_R) / sqrt(pow((AcY / A_R), 2) + pow((AcZ / A_R), 2))) * RAD_TO_DEG;
+  Acc[0] = atan((AcY / A_R) / sqrt(pow((AcX / A_R), 2) + pow((AcZ / A_R), 2))) * RAD_TO_DEG;
+
+  // Leer los valores del Giroscopio
+  Wire.beginTransmission(MPU);
+  Wire.write(0x43);
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU, 6, true);       // A partir del 0x43, se piden 6 registros
+  GyX = Wire.read() << 8 | Wire.read(); // Cada valor ocupa 2 registros
+  GyY = Wire.read() << 8 | Wire.read();
+  GyZ = Wire.read() << 8 | Wire.read();
+
+  // Calculo del angulo del Giroscopio
+  Gy[0] = GyX / G_R;
+  Gy[1] = GyY / G_R;
+  Gy[2] = GyZ / G_R;
+
+  dt = (millis() - timestampWire) / 1000.0;
+  timestampWire = millis();
+
+  // Aplicar el Filtro Complementario
+  Angle[0] = 0.98 * (Angle[0] + Gy[0] * dt) + 0.02 * Acc[0];
+  Angle[1] = 0.98 * (Angle[1] + Gy[1] * dt) + 0.02 * Acc[1];
+
+  // Integración respecto del tiempo paras calcular el YAW
+  Angle[2] = Angle[2] + Gy[2] * dt;
+
+  float xValue = Angle[0];
+  float yValue = Angle[1];
+  float zValue = Angle[2];
+
+  int directionX = getAxisValue(xValue, -24, 20);
+  int directionY = getAxisValue(yValue, 0, 10);
 
   if (millis() > timestamp + speed)
   {
@@ -79,10 +154,9 @@ void loop()
   FastLED.show();
 }
 
-int getAxisValue(uint8_t pin, int originAxis, int axisThreshold)
+int getAxisValue(float raw, int originAxis, int axisThreshold)
 {
   int value = 0;
-  int raw = analogRead(pin);
 
   raw < originAxis - axisThreshold ? value = 1 : 0;
   raw > originAxis + axisThreshold ? value = -1 : 0;
